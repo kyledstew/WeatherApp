c479b12043b9b8fe80db8ddbde9f41c6b31e9143
Kyle Stewart <kstewart@startstudio.com>
2021-02-11 09:21:50 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kyle Stewart on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
